ffe1a771b098b3d97123256e9c43b4877845bdf3
julietAlfaFoxtrotAlfaRomeo <jafarshodik036@gmail.com>
2024-05-30 18:08:19 +0700

    update halaman tailwinf dan componen

---
+++ resources/views/home.blade.php
@@ -0,0 +1,3 @@
+<x-layout-1>
+    <x-slot:title>{{$title}}</x-slot:title>
+</x-layout-1>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', ['title' => 'Home']);
+});
+
+Route::get('/porto', function () {
+    return view('porto', ['title' => 'Portofolio']);
+});
+
+Route::get('/contact', function () {
+    return view('contact', ['title' => 'contact']);
 });
